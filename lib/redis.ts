707b9374f983fa24d998d866fb6c87f28da9cddf
wjun2 <wjun2@linewell.com>
2023-08-31 00:17:09 +0800

    feat: upstash redis

---
+++ lib/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: `${process.env.UPSTASH_REDIS_REST_URL}`,
+  token: `${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+})
+
+export default redis
