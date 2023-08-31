68876e075fad41d700a472d7bbf6264a3e51d934
wjun2 <wjun2@linewell.com>
2023-08-31 22:34:18 +0800

    fix: repo 地址

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default async function Home() {
         >
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/steven-tey/precedent"
+            href="https://github.com/weijunext/nextjs-learn-demos"
             target="_blank"
             rel="noopener noreferrer"
           >
