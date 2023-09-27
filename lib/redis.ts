eff055a77b05a3772d1d9a635ccce57a79aa6438
WEI JUN <weijunext@gmail.com>
2023-09-27 10:28:55 +0800

    feat: 补充重试策略

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,6 +3,11 @@ import { Redis } from '@upstash/redis'
 const redis = new Redis({
   url: `${process.env.UPSTASH_REDIS_REST_URL}`,
   token: `${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+  // 重试策略
+  retry: {
+    retries: 5,
+    backoff: (retryCount) => Math.exp(retryCount) * 50,
+  },
 })
 
 export default redis
